6db3f80f38b8d001f51b82e21420a77402820068
621Matt <144735888+621Matt@users.noreply.github.com>
2023-09-19 09:45:44 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import turtle             # allows us to use the turtles library
+wn = turtle.Screen()      # creates a graphics window
+alex = turtle.Turtle()    # create a turtle named alex
+alex.forward(30)         # tell alex to move forward by 150 units
+alex.left(90)             # turn by 90 degrees
+alex.forward(100) 
+alex.left(90)
+alex.forward(100)
+alex.left(90)
+alex.forward(100)
+alex.left(90)
+alex.forward(75)
